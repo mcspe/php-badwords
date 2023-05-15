c7716f9c8595b80e00cc3d55aa528c151d60039a
mcspe <mcspezzaferro@gmail.com>
2023-05-15 19:56:25 +0200

    [first commit] set repo and page style

---
+++ cleaned-text.php
@@ -0,0 +1,41 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <!-- FAVICON -->
+  <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+  <!-- GOOGLE FONT -->
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@400;700&display=swap" rel="stylesheet">
+  <!-- BOOTSTRAP -->
+  <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.3/css/bootstrap.min.css' integrity='sha512-SbiR/eusphKoMVVXysTKG/7VseWii+Y3FdHrt0EpKgpToZeemhqHeZeLWLhJutz/2ut2Vw1uQEj2MbRF+TVBUA==' crossorigin='anonymous'/>
+  <!-- CUSTOM CSS -->
+  <link rel="stylesheet" href="style.css">
+  <title>Parental Advisor</title>
+</head>
+<body>
+  <header>
+    <div class="container-fluid d-flex align-items-center justify-content-center gap-5">
+      <img src="./logo.png" alt="LOGO">
+      <h1>KEEP YOUR TEXT CLEAN</h1>
+      <img src="./logo.png" alt="LOGO">
+    </div>
+  </header>
+  <main>
+    <div class="container my-5 text-center">
+      <h3>Here's your cleaned text!</h3>
+      <div class="ms-wrapper my-5">
+        
+      </div>
+    </div>
+  </main>
+  <footer></footer>
+</body>
+</html>
